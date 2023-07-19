7c3ecf56b539c7fee7252e07616a4d91d93ec376
klica123456 <maruck3@naver.com>
2023-07-19 10:22:13 +0900

    tt

--- hw_2_2.py
+++ hw_2_2.py
@@ -1,10 +1,15 @@
-book = '1'
-total = 10
-guide = '현재 보유 중인 총 책의 수는 다음과 같습니다.'
-print(guide)
-print(int(book) * total)
+a = 1
+b = 2
 
-changes = '그 중, 대여중인 책을 제외한 책의 수는 다음과 같습니다.'
-rental = 3.0
-print(changes)
-print(total - int(rental))
+def enclosed():
+    a = 10
+    c = 3
+
+    def local(c):
+        print(a, b, c) # 10 2 500
+
+    local(500)
+    print(a, b, c) # 10 2 3
+
+enclosed()
+print(a,b) # 1 2
